91d2df77ee99e57a67fc6dc3f5d27fe0426fbb07
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-10-11 15:39:16 +0200

    setup loadView and loadPartial

--- helpers.php
+++ helpers.php
@@ -4,3 +4,40 @@ function basePath(string $path): string
 {
     return __DIR__ . '/' . $path;
 }
+
+function loadView(string $name): void
+{
+    $viewPath = basePath("views/$name.view.php");
+    if (file_exists($viewPath)) {
+        require $viewPath;
+    } else {
+        echo "View not found: $viewPath";
+    }
+}
+
+
+function loadPartial(string $name): void
+{
+    $partialPath = basePath("views/partials/$name.php");
+
+    if (file_exists($partialPath)) {
+        require $partialPath;
+    } else {
+        echo "Partial not found: $partialPath";
+    }
+}
+
+function inspect(mixed $value): void
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+function inspectAndDie(mixed $value): void
+{
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+    die();
+}
--- public/index.php
+++ public/index.php
@@ -2,4 +2,4 @@
 
 require_once '../helpers.php';
 
-require_once basePath('views/home.views.php');
+loadView('home');
