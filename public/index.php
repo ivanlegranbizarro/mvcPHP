8827fca38acb422bb650f927343762f8ef336be5
Iván Legrán <ivanlegranbizarro@gmail.com>
2024-10-13 09:19:46 +0200

    separando routes $ router

--- public/index.php
+++ public/index.php
@@ -2,18 +2,7 @@
 
 require_once '../helpers.php';
 
-$routes = [
-    '/' => 'controllers/home.php',
-    '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
-];
-
 
 $uri = $_SERVER["REQUEST_URI"];
 
-if (array_key_exists($uri, $routes)) {
-    require(basePath($routes[$uri]));
-} else {
-    require(basePath('controllers/error/404.php'));
-}
+require_once basePath('router.php');
---
+++ router.php
@@ -0,0 +1,10 @@
+<?php
+
+$routes = require_once basePath('routes.php');
+
+if (array_key_exists($uri, $routes)) {
+    require(basePath($routes[$uri]));
+} else {
+    http_response_code(404);
+    require(basePath('controllers/error/404.php'));
+}
